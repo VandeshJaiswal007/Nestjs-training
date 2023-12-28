7e384be5efe6e97c01f96538c5891e0ac0e43f4d
VandeshJaiswal007 <vandesh.jaiswal@atriina.com>
2023-12-28 17:04:46 +0530

    added product and invoice using resource command

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,27 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  PostHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Put()
+  PutHello(): string {
+    return this.appService.getHello();
+  }
+
+
+  @Patch()
+  PatchHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Delete()
+  DeleteHello(): string {
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InvoiceModule } from './invoice/invoice.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [InvoiceModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
